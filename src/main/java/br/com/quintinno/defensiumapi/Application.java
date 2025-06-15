913adc938a0514ea8f01c20be65b4544b28ac874
José Quintinno <josequintino@hotmail.com.br>
2025-06-15 09:16:22 -0300

    init: Versão Inicial do Projeto

--- src/main/java/br/com/quintinno/defensiumapi/Application.java
+++ src/main/java/br/com/quintinno/defensiumapi/Application.java
@@ -1,13 +1,29 @@
 package br.com.quintinno.defensiumapi;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping
+	public String getMensagem() {
+		HashMap apiMap = new HashMap<>();
+			apiMap.put("APPLICATION", "DEFENSIUMAPI ");
+			apiMap.put("VERSION", "v1.0.0 ");
+			apiMap.put("PORT", "8080");
+		return apiMap.toString();
 	}
 
 }
