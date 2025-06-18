20a0fc14baa7c527b51328a1e9306e144b3c094b
José Quintinno <josequintino@hotmail.com.br>
2025-06-18 14:35:52 -0300

    build: Ajustes para implantação no ambiente de Produção

--- defensium_build.sh
+++ defensium_build.sh
@@ -1,6 +1,9 @@
 set -e
 
-git reset --hard && git fetch && git checkout main && git pull &
+git reset --hard > /dev/null 2>&1
+git fetch > /dev/null 2>&1
+git checkout main > /dev/null 2>&1
+git pull > /dev/null 2>&1 &
 
 nohup mvn clean install package -DskipTests > ../defensiumlog/mvn.log 2>&1 &
 
