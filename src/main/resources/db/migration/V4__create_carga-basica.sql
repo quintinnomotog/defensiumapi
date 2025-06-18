661651d30c669a021d0bdea28a82b4dc6e0b76e0
José Quintinno <josequintino@hotmail.com.br>
2025-06-18 12:16:05 -0300

    feature: Implementar migração de Carga Básica

---
+++ src/main/resources/db/migration/V4__create_carga-basica.sql
@@ -0,0 +1,19 @@
+/**
+ * Objetivo: 	Carga Básica de Dados (Para teste)
+ * Data: 		18/06/2025
+ * Demanda:		DEFENSIUMAPI20251806511455
+ */
+insert into tb_pessoa (code_public, nome, data_criacao, data_edicao, active) values (
+	'd93d1943-cb92-4dcb-b70c-7e4bb3978c8f',
+	'Google',
+	now(),
+	now(),
+	true
+);
+
+insert into tb_categoria_credencial (code_public, descricao, data_criacao, data_edicao) values (
+	'c93d1943-cb92-4dcb-b70c-7e4aa3978c3e',
+	'Conta Corporativa',
+	now(),
+	now()
+);
