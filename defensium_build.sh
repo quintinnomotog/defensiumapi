2c7d7f1119e0514126aeda5da2aa684bb6866983
José Quintinno <josequintino@hotmail.com.br>
2025-06-26 13:08:31 -0300

    build: Ajustes para implantação no ambiente de Homologação

--- defensium_build.sh
+++ defensium_build.sh
@@ -2,11 +2,8 @@ set -e
 
 git reset --hard > /dev/null 2>&1
 git fetch > /dev/null 2>&1
-git checkout main > /dev/null 2>&1
 git pull > /dev/null 2>&1 &
 
-nohup mvn clean install package -DskipTests > ../defensiumlog/mvn.log 2>&1 &
+nohup mvn clean install package -DskipTests > ../defensiumlog/mvn_v1.4.0.0.log 2>&1 &
 
-# nohup java -jar target/defensiumapi-0.0.1-SNAPSHOT.jar --spring.profiles.active=production > ../defensiumlog/log.txt 2>&1 &
-
-nohup java -jar target/defensiumapi-0.0.1-SNAPSHOT.jar > ../defensiumlog/log.txt 2>&1 &
+nohup java -jar target/defensiumapi-0.0.1-SNAPSHOT.jar > ../defensiumlog/mvn_v1.4.0.0.txt 2>&1 &
