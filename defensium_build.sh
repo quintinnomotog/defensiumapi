ae2580a8f62c4c6cf39ac75e36e0c657e2327e06
José Quintinno <josequintino@hotmail.com.br>
2025-06-26 16:27:48 -0300

    build: Ajustes para implantação no ambiente de Homologação

--- defensium_build.sh
+++ defensium_build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-set -e
+LOG_DIR="/data/data/com.termux/files/home/home/implantacao/Defensium/homologacao/defensiumlog"
 
-git reset --hard && git fetch && git pull
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 
-nohup mvn clean install package -DskipTests > ../defensiumlog/mvn_v1.4.0.0.log 2>&1 &
+LOG_MAVEN_FILE="MAVEN_${TIMESTAMP}.log"
+LOG_APPLICATION_FILE="DEFENSIUM_${TIMESTAMP}.log"
+
+LOG_MAVEN_PATH="$LOG_DIR/$LOG_MAVEN_FILE"
+LOG_APPLICATION_PATH="$LOG_DIR/$LOG_APPLICATION_FILE"
+
+git reset --hard && git pull
+
+nohup mvn clean install package -DskipTests > "$LOG_MAVEN_PATH" 2>&1 &
 
 wait
 
-nohup java -jar target/defensiumapi-0.0.1-SNAPSHOT.jar > ../defensiumlog/mvn_v1.4.0.0.txt 2>&1 &
+nohup java -jar target/defensiumapi-0.0.1-SNAPSHOT.jar > "$LOG_APPLICATION_PATH" 2>&1 &
+
+sleep 2
+
+tail -f "$LOG_APPLICATION_PATH"
