0055f5bc4f084a0b7d10623e83fbc426addacc9e
José Quintinno <josequintino@hotmail.com.br>
2025-07-04 18:40:25 -0300

    build: Configurar build automático no Servidor Termux (Homologação)

--- src/main/java/br/com/quintinno/defensiumapi/Application.java
+++ src/main/java/br/com/quintinno/defensiumapi/Application.java
@@ -1,9 +1,10 @@
 package br.com.quintinno.defensiumapi;
 
-import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.quintinno.defensiumapi.utility.DateUtility;
 
 @SpringBootApplication
 @RestController
@@ -20,22 +23,25 @@ public class Application implements CommandLineRunner {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
+	@Value("${spring.application.version}")
+	private String versao;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@GetMapping
-	public String getMensagem() {
-		HashMap apiMap = new HashMap<>();
-			apiMap.put("APPLICATION", "DEFENSIUMAPI");
-			apiMap.put("PORT", "8080");
-			apiMap.put("VERSION", "v1.4.0.0");
-		return apiMap.toString();
+	public Map<String, String> getMensagem() {
+		return Map.of(
+				"APPLICATION", "DEFENSIUMAPI",
+				"PORT", "8080",
+				"VERSION", versao,
+				"DATE", DateUtility.getDataHoraFormatada());
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.warn(getMensagem());
+		logger.warn(getMensagem().toString());
 	}
 
 }
